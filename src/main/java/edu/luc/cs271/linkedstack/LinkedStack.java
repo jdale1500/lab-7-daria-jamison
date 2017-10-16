6bf73b211f979039fe7c06516d9218fb8491cbd6
Daria Derda <dderda1@luc.edu>
2017-10-16 23:57:43 +0000

    small edits to linked stack

--- src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
+++ src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
@@ -9,18 +9,23 @@ public class LinkedStack<E> implements IStack<E> {
   /** The topmost node of this stack. The stack gets pushed down from here. */
   private Node<E> top;
 
-  // TODO why don't we need an explicit constructor?
+  // Done why don't we need an explicit constructor? Linked list has already been implemented
 
-  @Override
+  @Override //done
   public E push(final E obj) {
-    // TODO
-    return null;
+    top = new Node<> (obj, top);
+    return obj;
   }
 
   @Override
   public E peek() {
     // TODO
-    return null;
+    if(isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    else {
+      return top.data
+    }
   }
 
   @Override
